c85456117738f6f03311a881f8574ca35c8d0dba
Braden Harris <brharr@cs.csis.work>
2023-09-20 13:12:12 -0500

    Comment block added

--- VirtualPet.cpp
+++ VirtualPet.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************
+ * 	Braden Harris
+ * 	CS-355
+ * 	Assinment #7
+ * 	9/20/23
+ * 	Create a virtual pet class with attributes and methods. Then create
+ * 	  child classes that inherit and extend the virtual pet class.
+*/
+
 #include "VirtualPet.h"
 
 VirtualPet::VirtualPet(const string& petName) {
