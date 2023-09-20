671b5100b8d89d71d9c6f23be25c960edb14428e
Braden Harris <brharr@cs.csis.work>
2023-09-20 12:45:46 -0500

    Comment block added

--- Digimon.h
+++ Digimon.h
@@ -1,3 +1,13 @@
+/***************************************************************
+ * 	Braden Harris
+ * 	CS-355
+ * 	Assignment #7
+ * 	9/20/23
+ * 	Create a virtual pet class with attributes and methods.
+ * 	  Then create children that inherit and extend the 
+ * 	  virtual pet class.
+*/
+
 #ifndef DIGIMON_H
 #define DIGIMON_H
 #include "VirtualPet.h"
