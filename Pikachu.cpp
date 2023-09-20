f20b7a6f1c4222839d14ac92f1035e7eadbda4da
Braden Harris <brharr@cs.csis.work>
2023-09-20 13:02:25 -0500

    Comment block added

--- Pikachu.cpp
+++ Pikachu.cpp
@@ -1,7 +1,17 @@
+/*************************************************************************************
+ * 	Braden Harris
+ * 	CS-355
+ * 	Assignment #7
+ * 	9/20/23
+ * 	Create a virtual pet class with attributes and methods. Then create child
+ * 	  classes that inherit and extend the virtual pet class.
+*/
+
 #include "Pikachu.h"
 using namespace std;
 
-Pikachu::Pikachu(const string& pikaName, const string& pikaSpecies, int power): Pokemon(pikaName, pikaSpecies) {
+Pikachu::Pikachu(const string& pikaName, const string& pikaSpecies, int power): 
+     Pokemon(pikaName, pikaSpecies) {
 	electricPower = power;
 }
 
--- Pikachu.h
+++ Pikachu.h
@@ -1,3 +1,13 @@
+/********************************************************************
+ * 	Braden Harris
+ * 	CS-355
+ * 	Assinment #7
+ * 	9/20/23
+ * 	Create a virtual pet class with attributes and methods. Then 
+ * 	  create child classes that inherit and extend the virtual
+ * 	  pet class.
+*/
+
 #ifndef PIKACHU_H
 #define PIKACHU_H
 
